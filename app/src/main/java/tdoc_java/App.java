951a47d38ed44603419f1f0ca0bdab23a0940b7f
GHOST0O7 <ssr.22u10038@btech.nitdgp.ac.in>
2023-12-10 01:38:58 +0530

    small changes

--- app/src/main/java/tdoc_java/App.java
+++ app/src/main/java/tdoc_java/App.java
@@ -1,18 +1,9 @@
 package tdoc_java;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
