522964b041ea9d4d2f4102365a4645d9ab85a988
Sounak Das <sounak10das@gmail.com>
2023-12-11 05:40:30 +0530

    create tables implemented

---
+++ app/src/main/java/tdoc_java/Columns.java
@@ -0,0 +1,19 @@
+package tdoc_java;
+
+public class Columns {
+    private String name;
+    private String type;
+
+    public Columns(String name,String type){
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
