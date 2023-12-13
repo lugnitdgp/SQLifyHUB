c2748a9fa99dd3c9e9494120d3e9dcb6c6829113
GHOST0O7 <ssr.22u10038@btech.nitdgp.ac.in>
2023-12-13 17:58:53 +0530

    tried adding editing property

--- app/src/main/java/tdoc_java/Table.java
+++ app/src/main/java/tdoc_java/Table.java
@@ -1,8 +1,12 @@
 package tdoc_java;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 import javafx.scene.control.TableColumn;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.CheckBoxTableCell;
@@ -11,8 +15,10 @@ import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseEvent;
 import javafx.util.converter.BooleanStringConverter;
 import javafx.util.converter.NumberStringConverter;
+import javafx.scene.control.TableColumn.CellEditEvent;
 
-public class Table {
+
+public class Table implements Initializable{
 
     @FXML
     private Button addrow;
@@ -53,19 +59,46 @@ public class Table {
     
     public void addColumn(Columns col){ 
     TableColumn column = new TableColumn(col.getName());
-//    if(col.getType() == "character varying")
-//    column.setCellFactory(TextFieldTableCell.forTableColumn());
-//    else if(col.getType() == "integer")
-//    column.setCellFactory(TextFieldTableCell.forTableColumn(new NumberStringConverter()));
       Stringcounter++;
       String set = (char) (96+Stringcounter) + "";
       column.setCellFactory(TextFieldTableCell.forTableColumn());
       column.setCellValueFactory(new PropertyValueFactory<Cells,String>(set));
-      
-    base.getColumns().add(column);
+      column.setOnEditCommit(event->{
+        Cells cell = base.getSelectionModel().getSelectedItem();
+        int columnIndex = getColumnIndex(base, base.getSelectionModel().getSelectedCells().get(0));
+        switch(columnIndex)
+        {
+            case 0: cell.a = (String) event.getSource() ;break;
+            case 1: cell.b = (String) event.getSource();break;
+            case 2: cell.c = (String) event.getSource();break;
+            case 3: cell.d = (String) event.getSource();break;
+            case 4: cell.e = (String) event.getSource();break;
+            case 5: cell.f =(String) event.getSource();break;
+            case 6: cell.g = (String) event.getSource();break;
+            case 7: cell.h = (String) event.getSource();break;
+            case 8: cell.i = (String) event.getSource();break;
+            case 9: cell.j = (String) event.getSource();break;
+            case 10: cell.k = (String) event.getSource();break;
+            case 11: cell.l = (String) event.getSource();break;
+        }
+        base.refresh();
+      });
+      column.setEditable(true);
+      base.getColumns().add(column);
+    }
+    
+    private int getColumnIndex(TableView<?> tableView, TablePosition<?, ?> tablePosition) {
+        // Get the column index of the selected cell
+        return tableView.getColumns().indexOf(tablePosition.getTableColumn());
     }
     
     public void addRow(Cells row){
         base.getItems().add(row);
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        base.setEditable(true);
+    }
 }
+
